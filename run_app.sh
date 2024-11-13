948c5142c0b8e094b7645e935d8564473269b4bb
kilan <kilandav@yahoo.co.uk>
2024-11-13 21:35:27 +0000

    script to run locally

---
+++ run_app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export SPRING_DATASOURCE_URL=jdbc:postgresql://localhost:5432/testdb
+export PEXELS_API_KEY=<YOUR_PEXELS_API_KEY>
+export SPRING_DATASOURCE_USERNAME=testuser
+export SPRING_DATASOURCE_PASSWORD=testpassword
+export SPRING_JPA_HIBERNATE_DDL_AUTO=update
+export SPRING_CLOUD_AWS_REGION_STATIC=us-east-1
+export SPRING_CLOUD_AWS_CREDENTIALS_ACCESS_KEY=test
+export SPRING_CLOUD_AWS_CREDENTIALS_SECRET_KEY=test
+export SPRING_CLOUD_AWS_SQS_ENDPOINT=http://localhost:4566
+export SPRING_CLOUD_AWS_SES_ENDPOINT=http://localhost:4566
+export SOURCE_EMAIL=notifications@email.com
+export NOTIFICATION_SERVICE_QUEUE_URL=http://localhost:4566/000000000000/notification-service-queue
+
+export SERVER_PORT=8081
+
+mvn spring-boot:run
